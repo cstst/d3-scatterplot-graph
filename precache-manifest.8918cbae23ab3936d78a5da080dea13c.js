08528e7425c0ee39c9817a4d16ffc989bc367738
Derek L <derekslarson@gmail.com>
2018-10-18 12:02:15 +0700

    Updates

---
+++ precache-manifest.8918cbae23ab3936d78a5da080dea13c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38248adb8355c5846c93",
+    "url": "/d3-scatterplot-graph/static/js/runtime~main.38248adb.js"
+  },
+  {
+    "revision": "cc053449824fc74c8dc5",
+    "url": "/d3-scatterplot-graph/static/js/main.cc053449.chunk.js"
+  },
+  {
+    "revision": "94585ee9da81165cc142",
+    "url": "/d3-scatterplot-graph/static/js/1.94585ee9.chunk.js"
+  },
+  {
+    "revision": "cc053449824fc74c8dc5",
+    "url": "/d3-scatterplot-graph/static/css/main.d40c80cc.chunk.css"
+  },
+  {
+    "revision": "1389aa910ee5b2a654bccd03a333eba4",
+    "url": "/d3-scatterplot-graph/index.html"
+  }
+];
